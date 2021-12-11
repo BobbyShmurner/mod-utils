5e43ae43e40a67f47f03206d35067f15f61780d7
Ohdeymus <glitched.ohdeymus@gmail.com>
2021-12-11 10:29:46 +0000

    first commit

---
+++ include/main.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+#include "beatsaber-hook/shared/utils/logging.hpp"
+#include "beatsaber-hook/shared/config/config-utils.hpp"
+#include "beatsaber-hook/shared/utils/il2cpp-functions.hpp"
+
+Logger& getLogger();
